2310aad67d1538ce80f6ba0953476911c6477380
yinghui.zhao <sishenlail12@gmail.com>
2022-02-24 09:08:36 +0800

    feat: 添加RN脚手架

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleQuote: true,
+  jsxBracketSameLine: false,
+  trailingComma: 'none',
+  // arrowParens: 'avoid',
+  semi: false,
+  tabWidth: 4,
+  useTabs: false,
+  endOfLine: 'auto' 
+};
